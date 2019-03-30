2be5d979a6cee51ad5a8b889f19f5c9ed28ad8c2
AG51924 <AG51924@ISW42SLPC2668.ind.zensar.com>
2019-03-30 12:45:53 +0530

    modification

--- TestServlet/src/com/zensar/demo/logout.java
+++ TestServlet/src/com/zensar/demo/logout.java
@@ -24,6 +24,7 @@ public class logout extends HttpServlet {
 		}
 		response.sendRedirect("login.jsp");
 	}
+	
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
